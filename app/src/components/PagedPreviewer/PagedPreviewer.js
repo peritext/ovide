de597251a03f80f6d2655e391815ec7446cd9433
Robin de Mourat <robin.demourat@gmail.com>
2019-11-10 08:31:14 +0100

    improve paged previewer performance

--- app/src/components/PagedPreviewer/PagedPreviewer.js
+++ app/src/components/PagedPreviewer/PagedPreviewer.js
@@ -22,6 +22,7 @@ class PreviewWrapper extends Component {
     super( props );
     this.state = {
     };
+    this.frameRef = React.createRef();
   }
 
   componentDidMount = () => {
@@ -34,9 +35,74 @@ class PreviewWrapper extends Component {
       } );
   }
 
+  componentDidCatch = ( error, errorInfo ) => {
+    console.error( 'previewer catched an error', error, errorInfo );
+  }
+
   shouldComponentUpdate = ( { updateTrigger }, { pagedScript } ) => {
     return updateTrigger !== this.props.updateTrigger || this.state.pagedScript !== pagedScript;
   }
+
+  componentDidUpdate = () => {
+    const { props: { additionalHTML = '' } } = this;
+    if ( this.state.pagedScript && this.state.pagedScript.length ) {
+      this.injectRenderer( this.frameDocument, additionalHTML );
+    }
+  }
+
+  injectRenderer = ( thatDocument, additionalHTML ) => {
+    console.info( 'inject renderer' );/* eslint no-console: 0 */
+    // 1. swap body content to have sections as direct children (paged js requirement)
+    const container = thatDocument.body.children[0].querySelector( '.frame-content > div' );
+    let additionalStyles = '';
+    if ( container ) {
+      let htmlContent = container.innerHTML;
+      // thatDocument.body.innerHTML = htmlContent;
+
+      // 2. extract styles from content
+      const stylesRegexp = /<style.*>([\w\W\n]*)<\/style>/gm;
+      let match;
+        while ( ( match = stylesRegexp.exec( htmlContent ) ) !== null ) {
+          additionalStyles += match[1];
+          htmlContent = htmlContent.slice( 0, match.index ) + htmlContent.slice( match.index + match[0].length );
+          match.index = -1;
+      }
+      thatDocument.body.innerHTML = htmlContent;
+    }
+    if ( thatDocument.body.lastChild && !thatDocument.body.lastChild.tagName ) {
+      thatDocument.body.lastChild.remove();
+    }
+
+    // add toaster lib
+    const toasterScript = thatDocument.createElement( 'script' );
+    toasterScript.type = 'text/javascript';
+    toasterScript.src = 'https://cdn.jsdelivr.net/npm/toastify-js';
+    thatDocument.getElementsByTagName( 'head' )[0].appendChild( toasterScript );
+    const toasterStyle = thatDocument.createElement( 'link' );
+    toasterStyle.rel = 'stylesheet';
+    toasterStyle.type = 'text/css';
+    toasterStyle.href = 'https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css';
+    thatDocument.getElementsByTagName( 'head' )[0].appendChild( toasterStyle );
+
+    // load paged js lib
+    const pagedLibScript = thatDocument.createElement( 'script' );
+    pagedLibScript.type = 'text/javascript';
+    pagedLibScript.innerHTML = this.state.pagedScript || '';
+    // pagedLibScript.src = 'https://unpkg.com/pagedjs@0.1.30/dist/paged.polyfill.js';
+    thatDocument.getElementsByTagName( 'head' )[0].appendChild( pagedLibScript );
+    // load addons script
+    const addonsScript = thatDocument.createElement( 'script' );
+    addonsScript.type = 'text/javascript';
+    addonsScript.innerHTML = addons;
+    thatDocument.getElementsByTagName( 'head' )[0].appendChild( addonsScript );
+    // load paged js preview
+    const previewStyle = thatDocument.createElement( 'style' );
+    previewStyle.innerHTML = `${previewStyleData}
+        ${additionalStyles}`;
+    thatDocument.getElementsByTagName( 'head' )[0].appendChild( previewStyle );
+
+    thatDocument.head.innerHTML = thatDocument.head.innerHTML + additionalHTML;
+  };
 
   render = () => {
 
@@ -44,69 +110,12 @@ class PreviewWrapper extends Component {
       props: {
         style,
         Component: RenderingComponent,
-        additionalHTML = '',
       }
     } = this;
 
-    const injectRenderer = ( thatDocument ) => {
-      // 1. swap body content to have sections as direct children (paged js requirement)
-      const container = thatDocument.body.children[0].querySelector( '.frame-content > div' );
-      let additionalStyles = '';
-      if ( container ) {
-        let htmlContent = container.innerHTML;
-        // thatDocument.body.innerHTML = htmlContent;
-
-        // 2. extract styles from content
-        const stylesRegexp = /<style.*>([\w\W\n]*)<\/style>/gm;
-        let match;
-          while ( ( match = stylesRegexp.exec( htmlContent ) ) !== null ) {
-            additionalStyles += match[1];
-            htmlContent = htmlContent.slice( 0, match.index ) + htmlContent.slice( match.index + match[0].length );
-            match.index = -1;
-        }
-        thatDocument.body.innerHTML = htmlContent;
-      }
-      if ( thatDocument.body.lastChild && !thatDocument.body.lastChild.tagName ) {
-        thatDocument.body.lastChild.remove();
-      }
-
-      // add toaster lib
-      const toasterScript = thatDocument.createElement( 'script' );
-      toasterScript.type = 'text/javascript';
-      toasterScript.src = 'https://cdn.jsdelivr.net/npm/toastify-js';
-      thatDocument.getElementsByTagName( 'head' )[0].appendChild( toasterScript );
-      const toasterStyle = thatDocument.createElement( 'link' );
-      toasterStyle.rel = 'stylesheet';
-      toasterStyle.type = 'text/css';
-      toasterStyle.href = 'https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css';
-      thatDocument.getElementsByTagName( 'head' )[0].appendChild( toasterStyle );
-
-      // load paged js lib
-      const pagedLibScript = thatDocument.createElement( 'script' );
-      pagedLibScript.type = 'text/javascript';
-      pagedLibScript.innerHTML = this.state.pagedScript || '';
-      // pagedLibScript.src = 'https://unpkg.com/pagedjs@0.1.30/dist/paged.polyfill.js';
-      thatDocument.getElementsByTagName( 'head' )[0].appendChild( pagedLibScript );
-      // load addons script
-      const addonsScript = thatDocument.createElement( 'script' );
-      addonsScript.type = 'text/javascript';
-      addonsScript.innerHTML = addons;
-      thatDocument.getElementsByTagName( 'head' )[0].appendChild( addonsScript );
-      // load paged js preview
-      const previewStyle = thatDocument.createElement( 'style' );
-      previewStyle.innerHTML = `${previewStyleData}
-          ${additionalStyles}`;
-      thatDocument.getElementsByTagName( 'head' )[0].appendChild( previewStyle );
-
-      thatDocument.head.innerHTML = thatDocument.head.innerHTML + additionalHTML;
-
-      /*
-       * const additionalHTMLElement = thatDocument.createElement( 'div' );
-       * additionalHTMLElement.innerHTML = additionalHTML;
-       * thatDocument.getElementsByTagName( 'head' )[0].appendChild( additionalHTMLElement );
-       */
+    const dispatchContext = ( document/*, window*/ ) => {
+      this.frameDocument = document;
     };
-
     return (
       <div
         style={ style }
@@ -116,6 +125,7 @@ class PreviewWrapper extends Component {
           name={ 'preview' }
           id={ 'preview' }
           style={ { width: '100%', height: '100%' } }
+          ref={ this.frameRef }
         >
           <FrameContextConsumer>
             {( { document, window } ) => (
@@ -124,8 +134,9 @@ class PreviewWrapper extends Component {
                   { ...{ document, window } }
                   renderAdditionalHTML={ false }
                 />
+                {dispatchContext( document, window )}
                 {
-                    setTimeout( () => injectRenderer( document ) )
+                    // setTimeout( () => this.injectRenderer( document, additionalHTML ) )
                   }
               </div>
               )}
